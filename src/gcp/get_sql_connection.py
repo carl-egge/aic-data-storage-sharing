b7bdd32f77cce43296fbd679c1bf1e98db13ca88
Ahmad Hawat <ahmad.hawat@tuhh.de>
2023-05-31 16:30:09 +0200

    change from configparser to dotenv pkg

--- src/gcp/get_sql_connection.py
+++ src/gcp/get_sql_connection.py
@@ -7,41 +7,43 @@
 import os, sys
 from google.cloud.sql.connector import Connector
 import sqlalchemy
-import configparser
+from dotenv import load_dotenv
 
-# GCP IAM authentication using service account key
-# TODO: In order for this script to work you need to create a service account key, store the JSON and update the path below!
-# How to create a service account key: https://cloud.google.com/iam/docs/keys-create-delete?hl=de#iam-service-account-keys-create-console
-# Alternatively you can authenticate using the Google Cloud SDK.
-credential_path = "/home/carl-egge/aic/aic23-groupb1-data-storage-4f89199ed283.json" # IMPORTANT: Change this to your own path
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
-
-# set config
-# TODO: The .env file with the database credentials is needed for this to work.
-parser = configparser.ConfigParser()
-parser.read(".env")
-
-project_id = parser.get("DEFAULT", "project_id")
-region = parser.get("DEFAULT", "region")
-instance_name = parser.get("DEFAULT", "instance_name")
-db_user = parser.get("DEFAULT", "db_user")
-db_pass = parser.get("DEFAULT", "db_pass")
-db_name = parser.get("DEFAULT", "db_name")
-
-# initialize parameters
-INSTANCE_CONNECTION_NAME = f"{project_id}:{region}:{instance_name}"
-print(f"Your instance connection name is: {INSTANCE_CONNECTION_NAME}")
-
-# initialize Connector object
-connector = Connector()
 
 # function to return the database connection
 def getconn():
-    conn = connector.connect(
-        INSTANCE_CONNECTION_NAME,
-        "pymysql",
-        user=db_user,
-        password=db_pass,
-        db=db_name
-    )
-    return conn
+
+	# GCP IAM authentication using service account key
+	# In order for this script to work you need to create a service account key, store the JSON and update the path below!
+	# How to create a service account key: https://cloud.google.com/iam/docs/keys-create-delete?hl=de#iam-service-account-keys-create-console
+	# Alternatively you can authenticate using the Google Cloud SDK.
+	credential_path = "/home/pi/aic/gcp_key/aic23-groupb1-data-storage-4f89199ed283.json" # IMPORTANT: Change this to your own path
+	os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
+	# set config
+	# The .env file with the database credentials is needed for this to work.
+	load_dotenv()
+
+	project_id = os.getenv("project_id")
+	region = os.getenv("region")
+	instance_name = os.getenv("instance_name")
+	db_user = os.getenv("db_user")
+	db_pass = os.getenv("db_pass")
+	db_name = os.get("db_name")
+
+	# initialize parameters
+	INSTANCE_CONNECTION_NAME = f"{project_id}:{region}:{instance_name}"
+	print(f"Your instance connection name is: {INSTANCE_CONNECTION_NAME}")
+
+	# initialize Connector object
+	connector = Connector()
+
+	# Establish connection
+	conn = connector.connect(
+		INSTANCE_CONNECTION_NAME,
+		"pymysql",
+		user=db_user,
+		password=db_pass,
+		db=db_name
+	)
+	return conn
