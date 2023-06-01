3728af3b756fba59ec4ce3bb8eaea179f9dc89cc
ckf8282 <mohammad.rayhanur.rahman@nithh.de>
2023-06-01 09:15:07 +0200

    added encryption key reading function

--- src/encryption/symmetric.py
+++ src/encryption/symmetric.py
@@ -8,17 +8,21 @@
 import cryptography
 from cryptography.fernet import Fernet
 
-
 # This function generates a new key and stores it in the filesystem
 def generate_key():
     return Fernet.generate_key()
 
-encryption_key = generate_key()
-
 file_path = "../encryption_key_storage.txt"
 
-with open(file_path, "w") as texttxt:
-    texttxt.write(str(generate_key()))
+def write_key():
+    with open(file_path, "w") as texttxt:
+        texttxt.write(str(generate_key()))
+
+# This function reads the key from the filesystem
+def read_key():
+    with open(file_path, "r") as file:
+        encryption_key = file.read()
+    return str(encryption_key)
 
 # This function encrypts the data with the key and returns
 # the encrypted data as a string
@@ -27,14 +31,9 @@ def encrypt_data(data, key):
     encrypted_data = cipher_suite.encrypt(data.encode())
     return encrypted_data
 
-
 # This function decrypts the data with the key and returns
 # the decrypted data as a string
 def decrypt_data(data, key):
     cipher_suite = Fernet(key)
     decrypted_data = cipher_suite.decrypt(data)
     return decrypted_data
-
-# This function reads the key from the filesystem
-def read_key():
-    return encryption_key
