f35b1032096d88f1bdbdc820fd00464aa10fa129
Carl Egge <carl.egge@web.de>
2023-06-23 12:51:36 +0200

    add dynamic output to ui and update readme

--- src/app.py
+++ src/app.py
@@ -10,6 +10,8 @@ app.secret_key = b'_5#x3K"F5Y8z\n\xec]/'
 # Currently we store the batch size in a global variable
 # TODO: Store the batch size in a config file
 batch_size = 10
+# TODO: Don't store the shared secret in a global variable
+shared_secret = 'NaN'
 
 @app.context_processor
 def inject_load():
@@ -20,9 +22,20 @@ def inject_load():
     '''
     load = [int(random.random() * 100) / 100 for _ in range(3)]
     return { 
-        'temp': {'time': load[0], 'value': load[1], 'desc': load[2]},
+        'temp': {'time': '1970-01-01T00:00:00', 'value': load[1], 'desc': 'Cold'},
         'humi': {'time': load[0], 'value': load[1], 'desc': load[2]},
         'poll': {'time': load[0], 'value': load[1], 'desc': load[2]}
+    }
+
+
+@app.context_processor
+def inject_load():
+    '''
+    Injects the output of the data sharing and data storage functions into the template context
+    TODO: Replace with real data
+    '''
+    return { 
+        'output': 'Here we could display the output of the data sharing and data storage functions'
     }
 
 
@@ -31,12 +44,42 @@ def index():
     '''
     Renders the index.html template (home page)
     '''
-    return render_template('index.html', batch_size=batch_size)
+    return render_template('index.html', batch_size=batch_size, shared_secret=shared_secret)
 
 ##########################################################################################
 # The following routes are called with POST requests from the forms on the home page
 # They are used to actually control and run the application
 # TODO: Implement the actual functionality
+
+@app.route('/store-data', methods=['POST'])
+def store_data():
+    '''
+    Stores the sensor data in the GCP instance
+    '''
+    flash('Not implemented yet')
+    # flash('Success! Data stored in GCP')
+    return redirect("/")
+
+
+@app.route('/retrieve-data', methods=['POST'])
+def retrieve_data():
+    '''
+    Retrieves the sensor data from the GCP instance
+    '''
+    flash('Not implemented yet')
+    # flash('Success! Data retrieved from GCP')
+    return redirect("/")
+
+
+@app.route('/share-data', methods=['POST'])
+def share_data():
+    '''
+    Shares the sensor data with other peer device
+    '''
+    flash('Not implemented yet')
+    # flash('Success! Data shared with other peer')
+    return redirect("/")
+
 
 @app.route('/set-batch-size', methods=['POST'])
 def set_batch_size():
@@ -72,7 +115,19 @@ def connect_to_gcp():
     # flash('Success! Connected to GCP')
     return redirect("/")
 
+
+@app.route('/exchange-keys', methods=['POST'])
+def exchange_keys():
+    '''
+    Run asymmetric key exchange through mqtt and calculate the shared secret
+    '''
+    flash('Not implemented yet')
+    # flash('Success! Shared secret calculated')
+    return redirect("/")
+
+
 ##########################################################################################
+
 def update_load():
     '''
     Updates the sensor data every second using turbo.js
@@ -81,6 +136,7 @@ def update_load():
         while True:
             time.sleep(1)
             turbo.push(turbo.replace(render_template('loaddata.html'), 'sensor'))
+            turbo.push(turbo.replace(render_template('output.html'), 'output'))
 
 
 th = threading.Thread(target=update_load)
