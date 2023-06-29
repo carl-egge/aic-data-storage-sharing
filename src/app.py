ed88adf02bbbb78accff2cdec9ffdad33cb6435b
Ahmad Hawat <ahmad.hawat@tuhh.de>
2023-06-29 14:01:46 +0200

    Upload data

--- src/app.py
+++ src/app.py
@@ -4,6 +4,81 @@ from turbo_flask import Turbo
 import grovepi 
 import os, sys, signal, math
 from datetime import datetime
+import os
+from google.cloud.sql.connector import Connector
+from dotenv import load_dotenv
+import sqlalchemy
+from cryptography.fernet import Fernet
+
+
+
+# This function generates a new key and stores it in the filesystem
+def generate_encryption_key():
+    encryption_key = Fernet.generate_key()
+    encoded_key = encryption_key.decode()  # Convert bytes to string
+    with open("../encryption_key_storage.txt", "w") as texttxt:
+        texttxt.write(encoded_key)
+
+# This function reads the key from the filesystem
+def read_key():
+    with open("../encryption_key_storage.txt", "r") as file:
+        encoded_key = file.read()
+        encryption_key = encoded_key.encode() # Convert string to bytes
+    return encryption_key
+
+# This function encrypts the data with the key and returns
+# the encrypted data as a string
+def encrypt_data(data, key):
+    cipher_suite = Fernet(key)
+    encrypted_data = cipher_suite.encrypt(data.encode())
+    return encrypted_data
+
+# This function decrypts the data with the key and returns
+# the decrypted data as a string
+def decrypt_data(data, key):
+    cipher_suite = Fernet(key)
+    decrypted_data = cipher_suite.decrypt(data)
+    return decrypted_data
+
+
+# function to return the database connection
+def getconn():
+
+	# GCP IAM authentication using service account key
+	# In order for this script to work you need to create a service account key, store the JSON and update the path below!
+	# How to create a service account key: https://cloud.google.com/iam/docs/keys-create-delete?hl=de#iam-service-account-keys-create-console
+	# Alternatively you can authenticate using the Google Cloud SDK.
+	credential_path = "/home/pi/aic/gcp_key/aic23-groupb1-data-storage-4f89199ed283.json" # IMPORTANT: Change this to your own path
+	os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
+	# set config
+	# The .env file with the database credentials is needed for this to work.
+	load_dotenv()
+
+	project_id = os.getenv("project_id")
+	region = os.getenv("region")
+	instance_name = os.getenv("instance_name")
+	db_user = os.getenv("db_user")
+	db_pass = os.getenv("db_pass")
+	db_name = os.getenv("db_name")
+
+	# initialize parameters
+	INSTANCE_CONNECTION_NAME = f"{project_id}:{region}:{instance_name}"
+	print(f"Your instance connection name is: {INSTANCE_CONNECTION_NAME}")
+
+	# initialize Connector object
+	connector = Connector()
+
+	# Establish connection
+	conn = connector.connect(
+		INSTANCE_CONNECTION_NAME,
+		"pymysql",
+		user=db_user,
+		password=db_pass,
+		db=db_name
+	)
+	return conn
+
 
 sensor = 4  # The Sensor goes on digital port 4.
 
@@ -55,17 +130,6 @@ def inject_load():
     }
 
 
-@app.context_processor
-def inject_load():
-    '''
-    Injects the output of the data sharing and data storage functions into the template context
-    TODO: Replace with real data
-    '''
-    return { 
-        'output': 'Here we could display the output of the data sharing and data storage functions'
-    }
-
-
 @app.route('/')
 def index():
     '''
@@ -82,20 +146,62 @@ def index():
 def store_data():
     '''
     Stores the sensor data in the GCP instance
+    
+    sensor_data = inject_load()
+    string_data = str(sensor_data)
+    encrypted_data_string = encrypt_data(string_data, read_key())
+    app.config['encrypted_data_string'] = encrypted_data_string  # Store decrypted_data in app context
     '''
     flash('Not implemented yet')
-    # flash('Success! Data stored in GCP')
     return redirect("/")
 
 
+'''
+@app.context_processor
+def inject_load():
+   
+    Injects the output of the data sharing and data storage functions into the template context
+    TODO: Replace with real data
+    
+    encrypted_data_string = app.config.get('encrypted_data_string')
+    
+    return {
+        'output': encrypted_data_stringy
+    }
+'''
 @app.route('/retrieve-data', methods=['POST'])
 def retrieve_data():
     '''
     Retrieves the sensor data from the GCP instance
     '''
-    flash('Not implemented yet')
-    # flash('Success! Data retrieved from GCP')
+    pool = sqlalchemy.create_engine(
+        "mysql+pymysql://",
+        creator=getconn,
+    )
+    with pool.connect() as db_conn:
+        # query and fetch test table
+        results = db_conn.execute(sqlalchemy.text("SELECT * FROM sensors_data")).fetchall()
+
+        last_row = results[-1]
+        token = last_row[1].encode()  # Convert token to bytes
+        decrypted_data = decrypt_data(token, read_key())
+
+        app.config['decrypted_data'] = decrypted_data  # Store decrypted_data in app context
+
     return redirect("/")
+
+
+@app.context_processor
+def inject_load():
+    '''
+    Injects the output of the data sharing and data storage functions into the template context
+    TODO: Replace with real data
+    '''
+    decrypted_data = app.config.get('decrypted_data')  # Retrieve decrypted data from app context
+    return {
+        'output': decrypted_data 
+    }
+
 
 
 @app.route('/share-data', methods=['POST'])
